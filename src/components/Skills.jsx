f84112165fa4cca3ee900a7f2fe8360e93b69ce2
Amy Jung <ajwjung@outlook.com>
2023-09-25 17:50:30 -0700

    Add buttons and hover effects

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import "../styles/form.css"
 import { useState } from "react";
 import { SkillsFields } from "./FormFields";
 
-function SkillsSection({ skillsInfo }) {
+function SkillsSection({ skillsInfo, handleMouseEnter, handleMouseLeave, buttonHoverStyle, entryHoverStyle }) {
     return (
         <section className="skillsWrapper">
             <ul>
@@ -10,10 +10,45 @@ function SkillsSection({ skillsInfo }) {
                     return (
                         <li 
                             className="skillsEntry"
+                            onMouseEnter={(e) => {
+                                const hoveredEntryId = e.target.closest(".skillsEntry").id.slice(-1);
+                                handleMouseEnter(Number(hoveredEntryId));
+                            }} 
+                            onMouseLeave={(e) => {
+                                const hoveredEntryId = e.target.closest(".skillsEntry").id.slice(-1);
+                                handleMouseLeave(Number(hoveredEntryId));
+                            }}
                             id={"skill" + index}
                             key={"skill" + index}
                         >
-                            <b>{entry.category}</b> {entry.listedSkills}
+                            <p 
+                                className="skillsDetails" 
+                                style={entryHoverStyle[index]}
+                            >
+                                <b>{entry.category}</b> {entry.listedSkills}
+                            </p>
+                            <div className="btns" style={buttonHoverStyle[index]}>
+                                <button
+                                    className="editInfo"
+                                    onClick={(e) => {
+                                        e.preventDefault()
+                                    }}
+                                    style={{margin: "10px"}}
+                                    type="button"
+                                >
+                                    Edit
+                                </button>
+                                <button
+                                    className="deleteInfo"
+                                    onClick={(e) => {
+                                        e.preventDefault()
+                                    }}
+                                    style={{margin: "10px"}}
+                                    type="button"
+                                >
+                                    Delete
+                                </button>
+                            </div>
                         </li>
                     )
                 })}
@@ -30,12 +65,62 @@ function Skills() {
             listedSkills: "High agility, decision-making, problem-solving"
         }
     ]);
+    const [buttonHoverStyle, setButtonHoverStyle] = useState([{ display: "none" }]);
+    const [entryHoverStyle, setEntryHoverStyle] = useState([{}]);
+
+    function handleMouseEnterSkill(entryId) {
+        setButtonHoverStyle(
+            buttonHoverStyle.map((buttonStyle, index) => {
+                if (index === entryId) {
+                    return ({ display: "block" });
+                } else {
+                    return buttonStyle;
+                }
+            })
+        );
+        setEntryHoverStyle(
+            entryHoverStyle.map((entryStyle, index) => {
+                if (index === entryId) {
+                    return ({ opacity: "50%" });
+                } else {
+                    return entryStyle;
+                }
+            })
+        )
+    }
+
+    function handleMouseLeaveSkill(entryId) {
+        setButtonHoverStyle(
+            buttonHoverStyle.map((buttonStyle, index) => {
+                if (index === entryId) {
+                    return ({ display: "none" });
+                } else {
+                    return buttonStyle;
+                }
+            })
+        );
+        setEntryHoverStyle(
+            entryHoverStyle.map((entryStyle, index) => {
+                if (index === entryId) {
+                    return ({});
+                } else {
+                    return entryStyle;
+                }
+            })
+        )
+    }
 
     return (
         <>
             <h2>Skills</h2>
             <SkillsFields />
-            <SkillsSection skillsInfo={skillsInfo} />
+            <SkillsSection 
+                skillsInfo={skillsInfo} 
+                handleMouseEnter={handleMouseEnterSkill}
+                handleMouseLeave={handleMouseLeaveSkill}
+                buttonHoverStyle={buttonHoverStyle}
+                entryHoverStyle={entryHoverStyle}
+            />
         </>
     )
 }
